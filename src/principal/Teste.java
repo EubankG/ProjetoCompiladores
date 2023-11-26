411c283e832c8fc0c93ebd0e3c58f06e56b68e94
BigJh13 <big.jh.13.2@gmail.com>
2023-11-26 18:36:52 -0300

    fix main

--- src/principal/Teste.java
+++ src/principal/Teste.java
@@ -10,28 +10,39 @@ import enumerator.TipoToken;
 public class Teste {
 	
 	public static void main(String[] args) {
-		String input = "INTEIRO teste = 2 + 22 * 2 - 1 + 1 + 5 + bcs;";
-        AnalisadorLexico analisador = new AnalisadorLexico(input);
+		String input = "INTEIRO teste = 2 + 22 * 2 + 2 - 1 ;";
         List<Token> tokens = new ArrayList<Token>();
 
-        Token token;
+        Token token = null;
         do {
-            token = analisador.nextToken();
-            System.out.println(token);
-            
-            tokens.add(token);
+            String[] palavras = input.split(" ");
+            for(String palavra : palavras){
+
+                AnalisadorLexico analisador = new AnalisadorLexico(palavra);
+
+                token = analisador.nextToken();
+                System.out.println(token);
+
+                tokens.add(token);
+            }
         } while (token.getTipo() != TipoToken.FIM);
 		
        
         //VALIDOU OS TOKENS
-        AnalisadorSintatico parse = new AnalisadorSintatico(tokens);
-        parse.parse();
-        
+        try {
+            AnalisadorSintatico parse = new AnalisadorSintatico(tokens);
+            parse.parse();
+        }catch (Exception e){
+
+        }
         
         //REALIZAR TAREFAS
-        AnalisadorSemantico semantico = new AnalisadorSemantico(tokens);
-        System.out.println(semantico.analizar());
-    
+        try {
+            AnalisadorSemantico semantico = new AnalisadorSemantico(tokens);
+            System.out.println(semantico.analizar());
+        }catch (Exception e){
+
+        }
 	}
 
 }
